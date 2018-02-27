31ec70e012c3f73a5a2b6d9efcf9a3f0f7787f2d
Frank H Mcmanus <frank@thefrankharvey.com>
2018-02-27 03:19:28 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -22,13 +22,14 @@ end
 def end_game(card_total)
 puts "Sorry, you hit #{card_total}. Thanks for playing!"end
 
-def initial_round()
+def initial_round
 card_total = deal_card + deal_card
 display_card_total(card_total)
 end
 
-def hit?
-  # code hit? here
+def hit?(card_total)
+prompt_user
+get_user_input
 end
 
 def invalid_command
